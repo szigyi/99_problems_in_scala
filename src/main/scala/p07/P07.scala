909b657c13a453bf51826bea7fb2c9c1894e8fa9
Szabolcs Szilagyi <szabolcs.szilagyi.hu@gmail.com>
2018-06-13 23:40:53 +0100

    Solved flattener. Used help.

--- src/main/scala/p07/P07.scala
+++ src/main/scala/p07/P07.scala
@@ -4,30 +4,13 @@ class P07 {
 
   def flatten(list: List[Any]): List[Any] = {
 
-    def iterator(remaining: List[Any], acc: List[Any]): List[Any] = remaining match {
+    def flattenIter(remaining: List[Any], acc: List[Any]): List[Any] = remaining match {
       case Nil => acc
-      case head :: tail => head match {
-        case List => iterator(head.asInstanceOf[List[Any]], acc)
-        case _ => {
-          val newAcc = head :: acc
-          iterator(tail, newAcc)
-        }
-      }
+      case (head: List[_]) :: Nil => flattenIter(head, acc)
+      case (head: List[_]) :: tail => flattenIter(tail, acc ::: head)
+      case head :: tail => flattenIter(tail, acc :+ head)
     }
-    iterator(list, List())
-
-
-//    def flattenIter(remaining: List[List[T]], acc: List[T]): List[T] = remaining match {
-//      case Nil => acc
-//      case head :: tail => flattenIter(tail, flat(head, acc))
-//    }
-//
-//    def flat(embeddedList: List[T], acc: List[T]): List[T] = embeddedList match {
-//      case Nil => acc
-//      case head :: tail => head :: flat(tail, acc)
-//    }
-//
-//    flattenIter(list, List())
+    flattenIter(list, List())
   }
 }
 
