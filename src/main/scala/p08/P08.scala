b0db9ab6b8b8224e7d1c2f6ff3cf7e290f9277e3
Szabolcs Szilagyi <szabolcs.szilagyi.hu@gmail.com>
2018-06-15 22:13:44 +0100

    Solved compression by recursion

--- src/main/scala/p08/P08.scala
+++ src/main/scala/p08/P08.scala
@@ -2,5 +2,18 @@ package p08
 
 class P08 {
 
+  def compress[T](list: List[T]): List[T] = {
+    def iter(remaining: List[T], prev: T, acc: List[T]): List[T] = remaining match {
+      case Nil => acc
+      case head :: tail => {
+        if (head.equals(prev)) iter(tail, head, acc) else iter(tail, head, acc :+ head)
+      }
+    }
+    if (list.isEmpty) return list
+    iter(list.tail, list.head, List(list.head))
+  }
+}
 
+object P08 {
+  def apply(): P08 = new P08()
 }
--- src/test/scala/p08/P08Test.scala
+++ src/test/scala/p08/P08Test.scala
@@ -4,4 +4,28 @@ import org.scalatest.FunSuite
 
 class P08Test extends FunSuite {
 
+  test("cannot compress empty list") {
+    assert(P08().compress(Nil).equals(Nil))
+  }
+
+  test("list without duplications is not compressed") {
+    val list = List(1, 2, 3, 4, 5, 6, 7)
+    val compressed = P08().compress(list)
+    println(s"2. $compressed")
+    assert(compressed.equals(list))
+  }
+
+  test("not consecutive duplicated elements are not compressed") {
+    val list = List(1, 2, 1, 3, 5, 4, 5, 1)
+    val compressed = P08().compress(list)
+    println(s"3. $compressed")
+    assert(compressed.equals(List(1, 2, 1, 3, 5, 4, 5, 1)))
+  }
+
+  test("consecutive duplicated elements are compressed") {
+    val list = List(1, 1, 2, 3 ,4, 5, 5, 1)
+    val compressed = P08().compress(list)
+    println(s"4. $compressed")
+    assert(compressed.equals(List(1, 2, 3, 4, 5, 1)))
+  }
 }
